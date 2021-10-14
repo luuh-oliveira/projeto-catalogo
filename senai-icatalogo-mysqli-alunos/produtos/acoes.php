dbe657fe7e8df46f629c2f955b3d6d0bf9c17eff
luuh-oliveira <lu.oliveira0505@gmail.com>
2021-10-13 23:57:25 -0300

    Adicionar produto

--- senai-icatalogo-mysqli-alunos/produtos/acoes.php
+++ senai-icatalogo-mysqli-alunos/produtos/acoes.php
@@ -22,6 +22,33 @@ switch ($_POST["acao"]) {
         // upload do arquivo
         move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/$novoNome");
 
+
+
+        //INSERÇÃO DE DADOS NA BASE DE DADOS DO MYSQL: 8/10
+
+        //RECEBIMENTO DOS DADOS:    
+        $descricao = $_POST["descricao"];
+        $peso = $_POST["peso"];
+        $quantidade = $_POST["quantidade"];
+        $cor = $_POST["cor"];
+        $tamanho = $_POST["tamanho"];
+        $valor = $_POST["valor"];
+        $desconto = $_POST["desconto"];
+        $categoria_id = $_POST["categoria"];
+
+        //CRIAÇÃO DA INSTRUÇÃO SQL DE INSERÇÃO:
+        $sql = "INSERT INTO tbl_produto
+        (descricao, peso, quantidade, cor, tamanho, valor, desconto, imagem, categoria_id) /*CAMPOS QUE IRÃO RECEBER*/
+        VALUES('$descricao', $peso, $quantidade, '$cor', '$tamanho', $valor , $desconto, '$novoNome', $categoria_id)";/*VALORES QUE OS CAMPOS IRÃO RECEBER */
+        
+        //OBS: AS VARIAVEIS QUE SÃO VARCHAR(TEXTO) SÃO ENVOLVIDAS POR ASPAS SIMPLES
+
+        //EXECUÇÃO DO SQL DE INSERÇÃO:
+        $resultado = mysqli_query($conexao, $sql);
+
+        //REDIRECIONAR PARA INDEX:
+        header('location: index.php');
+
         break;
     
     default:
