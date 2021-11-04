cf65b09f211950381339585ca9d72f7f43edc3e2
luuh-oliveira <lu.oliveira0505@gmail.com>
2021-11-04 11:16:55 -0300

    Finalização do projeto

--- senai-icatalogo-mysqli-alunos/componentes/header/acoesLogin.php
+++ senai-icatalogo-mysqli-alunos/componentes/header/acoesLogin.php
@@ -6,7 +6,7 @@ require("../../database/conexao.php");
 
 function realizarLogin($usuario, $senha, $conexao){
 
-    $sql = "SELECT * FROM tbl_administrador WHERE usuario = '$usuario' AND senha = '$senha'";
+    $sql = "SELECT * FROM tbl_administrador WHERE usuario = '$usuario'";
     $resultado = mysqli_query($conexao, $sql);
 
     $dadosUsuario = mysqli_fetch_array($resultado);
